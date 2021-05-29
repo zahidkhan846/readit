0fc123b16164b39754c82cc4c7410d53692bc915
Zahid Khan <zahid.khan846@hotmail.com>
2021-05-29 23:52:52 +0530

    SUB CLIENT

--- client/pages/r/[sub].tsx
+++ client/pages/r/[sub].tsx
@@ -1,28 +1,68 @@
+import classNames from "classnames";
 import Head from "next/head";
 import Image from "next/image";
 import { useRouter } from "next/router";
-import React, { Fragment } from "react";
+import React, { ChangeEvent, Fragment, useRef } from "react";
 import useSWR from "swr";
 import PostItem from "../../components/resources/post";
+import { axiosConnect } from "../../config/axios";
+import { useAuth } from "../../context/auth";
+import { Sub } from "../../utils/typeDefs";
 
-function Sub() {
+function SubPage() {
   const router = useRouter();
+
+  const { authenticated, user } = useAuth();
+
+  const imageUploadRef = useRef<HTMLInputElement>();
 
   const subName = router.query.sub;
 
-  const { data: sub, error } = useSWR(subName ? "/sub/" + subName : null);
+  const {
+    data: sub,
+    error,
+    revalidate,
+  } = useSWR<Sub>(subName ? "/sub/" + subName : null);
 
   if (!sub) {
     return (
-      <p className="flex items-center justify-center h-screen text-3xl font-bold text-green-500">
+      <p className="flex items-center justify-center h-screen text-3xl font-bold text-gray-500">
         Loading...
       </p>
     );
+  }
+  let subOwner;
+  if (authenticated && user.username === sub.username) {
+    subOwner = user.username;
   }
 
   if (error) {
     console.log(error);
   }
+
+  const openFileExplorer = (type: string) => {
+    if (!subOwner) {
+      return;
+    }
+    imageUploadRef.current.name = type;
+    imageUploadRef.current.click();
+  };
+
+  const uploadImage = async (e: ChangeEvent<HTMLInputElement>) => {
+    const file = e.target.files[0];
+    const formData = new FormData();
+    formData.append("file", file);
+    formData.append("type", imageUploadRef.current.name);
+
+    try {
+      await axiosConnect.post(`/sub/${sub.name}/image`, formData, {
+        headers: { "Content-Type": "multipart/form-data" },
+      });
+      revalidate();
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   return (
     <Fragment>
@@ -30,15 +70,50 @@ function Sub() {
         <title>{sub.title}</title>
       </Head>
       <div className="mt-12">
-        <header className="w-full h-40 bg-blue-800">
-          {sub.imageUrl && (
+        <input
+          type="file"
+          hidden={true}
+          ref={imageUploadRef}
+          onChange={uploadImage}
+        />
+        <header
+          onClick={() => openFileExplorer("banner")}
+          className={classNames("w-full h-60 bg-blue-800", {
+            "cursor-pointer": subOwner,
+          })}
+        >
+          {sub.bannerUrl && (
             <img
               className="object-cover w-full h-full"
-              src={sub.imageUrl}
+              src={sub.bannerUrl}
               alt={sub.title}
             />
           )}
         </header>
+        <div className="px-2 py-3 bg-white">
+          <div className="container flex items-center gap-4">
+            <div style={{ marginTop: "-20px" }}>
+              <Image
+                className={classNames("rounded-full", {
+                  "cursor-pointer": subOwner,
+                })}
+                src={sub.imageUrl}
+                height={80}
+                width={80}
+                onClick={() => openFileExplorer("image")}
+              />
+            </div>
+            <div className="flex flex-col">
+              <div className="flex items-center justify-center h-full gap-4">
+                <h1 className="text-2xl font-bold">{sub.title}</h1>
+                <button className="px-6 py-1 font-bold text-white bg-blue-500 border rounded-full focus:outline-none hover:bg-blue-400">
+                  Join
+                </button>
+              </div>
+              <p className="text-xs font-semibold text-gray-500">{`r/${sub.title.toLowerCase()}`}</p>
+            </div>
+          </div>
+        </div>
         <div className="container">
           <div className="flex gap-4 pt-4">
             <div className="flex-1">
@@ -54,4 +129,4 @@ function Sub() {
   );
 }
 
-export default Sub;
+export default SubPage;
